9db1709e30c686d0e27a3eec4d60b802dd9b6463
Tom <tom.pitkin@gmail.com>
2014-04-07 22:51:14 -0700

    Added code to load image, resize based on max pixel count, and partially convert pixel colors to powder game palette. Still adding more colors...

--- PowderPixelArt/Form1.Designer.cs
+++ PowderPixelArt/Form1.Designer.cs
@@ -31,22 +31,24 @@
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.bStart = new System.Windows.Forms.Button();
             this.bLoadImage = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox
             // 
+            this.pictureBox.BackColor = System.Drawing.Color.Black;
             this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox.Location = new System.Drawing.Point(12, 12);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(399, 299);
+            this.pictureBox.Size = new System.Drawing.Size(395, 295);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             // 
             // bStart
             // 
             this.bStart.Enabled = false;
-            this.bStart.Location = new System.Drawing.Point(336, 318);
+            this.bStart.Location = new System.Drawing.Point(332, 313);
             this.bStart.Name = "bStart";
             this.bStart.Size = new System.Drawing.Size(75, 23);
             this.bStart.TabIndex = 1;
@@ -55,18 +57,24 @@
             // 
             // bLoadImage
             // 
-            this.bLoadImage.Location = new System.Drawing.Point(255, 317);
+            this.bLoadImage.Location = new System.Drawing.Point(251, 313);
             this.bLoadImage.Name = "bLoadImage";
             this.bLoadImage.Size = new System.Drawing.Size(75, 23);
             this.bLoadImage.TabIndex = 2;
             this.bLoadImage.Text = "Load Image";
             this.bLoadImage.UseVisualStyleBackColor = true;
+            this.bLoadImage.Click += new System.EventHandler(this.bLoadImage_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "Image Files (*.bmp, *.jpg)|*.bmp;*.jpg";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 353);
+            this.ClientSize = new System.Drawing.Size(419, 342);
             this.Controls.Add(this.bLoadImage);
             this.Controls.Add(this.bStart);
             this.Controls.Add(this.pictureBox);
@@ -84,6 +92,7 @@
         private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Button bStart;
         private System.Windows.Forms.Button bLoadImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
 
--- PowderPixelArt/Form1.cs
+++ PowderPixelArt/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media.Imaging;
 
 namespace PowderPixelArt
 {
@@ -15,6 +17,89 @@ namespace PowderPixelArt
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private static Color powder = Color.FromArgb(242, 189, 107);
+        private static Color water = Color.FromArgb(64, 64, 255);
+        private static Color fire = Color.FromArgb(255, 64, 64);
+        private static Color seed = Color.FromArgb(144, 192, 64);
+        private static Color[] powderColors = new Color[] {powder, water, fire, seed};
+
+        private void bLoadImage_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream imageStreamSource = new FileStream(openFileDialog.FileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+                Image image = Image.FromStream(imageStreamSource);
+
+                Bitmap bmap = resizeImage(image, 40000);
+                    
+                const int maxImageWidth = 395;
+                const int maxImageHeight = 295;
+                if (bmap.Width > maxImageWidth)
+                {
+                    int newImageHeight = (int)(bmap.Height * ((float)maxImageWidth / (float)bmap.Width));
+                    bmap = new Bitmap(maxImageWidth, newImageHeight);
+                }
+                if (bmap.Height > maxImageHeight)
+                {
+                    int newImageWidth = (int)(bmap.Width * ((float)maxImageHeight / (float)bmap.Height));
+                    bmap = new Bitmap(newImageWidth, maxImageHeight);
+                }
+                Graphics graph = Graphics.FromImage(bmap);
+                graph.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graph.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                graph.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                graph.DrawImage(image, 0, 0, bmap.Width, bmap.Height);
+                applyColorPalette(bmap, powderColors);
+                pictureBox.Image = bmap;
+                bStart.Enabled = true;
+            }
+            else
+                bStart.Enabled = false;
+        }
+
+        private Bitmap resizeImage(Image image, int maxPixels)
+        {
+            int pixels = image.Width * image.Height;
+            if (pixels > maxPixels)
+            {
+                float ratio = image.Width / image.Height;
+                float scale = (float)Math.Sqrt((float)pixels / maxPixels);
+                return new Bitmap((int)(ratio * image.Width / scale), (int)(image.Height / scale));
+            }
+            return new Bitmap(image.Width, image.Height);
+        }
+
+        private void applyColorPalette(Bitmap image, Color[] pallete)
+        {
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixelColor = image.GetPixel(x, y);
+                    Color bestColor = Color.Black;
+                    double distance = Double.MaxValue;
+                    foreach( Color element in pallete)
+                    {
+                        double red = Math.Pow(Convert.ToDouble(element.R) - pixelColor.R, 2.0);
+                        double green = Math.Pow(Convert.ToDouble(element.G) - pixelColor.G, 2.0);
+                        double blue = Math.Pow(Convert.ToDouble(element.B) - pixelColor.B, 2.0);
+                        double tempDist = Math.Sqrt(red + green + blue);
+                        if (tempDist == 0.0)
+                        {
+                            bestColor = element;
+                            break;
+                        }
+                        else if (tempDist < distance)
+                        {
+                            distance = tempDist;
+                            bestColor = element;
+                        }
+                    }
+                    image.SetPixel(x, y, bestColor);
+                }
+            }
         }
     }
 }
